669dec552f3ec481d8715987ef1185dcfdd469ab
BillyDaWalrus <bill6399@gmail.com>
2020-06-06 22:05:44 -0400

    final

--- website_components/app.py
+++ website_components/app.py
@@ -3,48 +3,33 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-@app.route('/website_cover.html')
 def hello(name=None):
     return render_template('website_cover.html', name=name)
 
-
 @app.route('/McDonalds/')
-@app.route('/map/website_page_mcdonalds.html/')
 def map_McDonalds(name=None):
     return render_template('website_page_mcdonalds.html', name=name)
 
 @app.route('/Wendys/')
-@app.route('/map_wendys/website_page_wendys.html/')
 def map_Wendys(name=None):
     return render_template('website_page_wendys.html', name=name)
 
 @app.route('/KFC/')
-@app.route('/map/website_page_kfc.html/')
 def map_KFC(name=None):
     return render_template('website_page_kfc.html', name=name)
 
-<<<<<<< HEAD
 @app.route('/Arbys/')
-@app.route('/map/website_page_arbys.html/')
-def map_KFC(name=None):
+def map_Arbys(name=None):
     return render_template('website_page_arbys.html', name=name)
 
 @app.route('/Burger-King/')
-@app.route('/map/website_page_burgerking.html/')
-def map_KFC(name=None):
+def map_BurgerKing(name=None):
     return render_template('website_page_burgerking.html', name=name)
 
 @app.route('/In-N-Out/')
-@app.route('/map/website_page_in-Nout.html/')
-def map_KFC(name=None):
+def map_InNOut(name=None):
     return render_template('website_page_in-Nout.html', name=name)
 
 @app.route('/Chuck-E-Cheese/')
-@app.route('/map/website_page_chuck.html/')
-def map_KFC(name=None):
+def map_Chuck(name=None):
     return render_template('website_page_chuck.html', name=name)
-
-=======
->>>>>>> 5ab37c90ae79a07fb9c30f0ce8c2c7ba6275a343
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80)
